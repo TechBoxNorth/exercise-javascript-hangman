b15d9e7abac843cd3766af2c862b00d2565c5966
TechBoxNorth <techboxnorth@gmail.com>
2024-01-29 15:24:34 +0100

    corrected the id of the different parts in the svg-file

---
+++ index.js
@@ -0,0 +1,15 @@
+const noose = document.querySelector('#noose');
+const head = document.querySelector('#head');
+const body = document.querySelector('#body');
+const arms = document.querySelector('#arms');
+const legs = document.querySelector('#legs');
+const scaffold = document.querySelector('#scaffold');
+const ground = document.querySelector('#ground');
+
+noose.setAttribute('opacity', 1);
+head.setAttribute('opacity', 1);
+body.setAttribute('opacity', 1);
+arms.setAttribute('opacity', 1);
+legs.setAttribute('opacity', 1);
+scaffold.setAttribute('opacity', 1);
+ground.setAttribute('opacity', 1);
